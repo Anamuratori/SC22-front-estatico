f0951328be27ab8784fcb9612919937358abc8a1
Pietro Zardini <pizardini@gmail.com>
2023-06-09 18:35:10 -0300

    remoção do JS do index e adição de um arquivo separado

---
+++ script.js
@@ -0,0 +1,61 @@
+let botaoLikeC = document.getElementById("likeC");
+let numeroC = document.getElementById("contagemC");
+
+let countC = 2656;
+
+function incrementarC() {
+    countC++;
+    numeroC.innerText = countC
+}
+
+
+let botaoLikeSql = document.getElementById("likeSql")
+let numeroSql = document.getElementById("contagemSql")
+
+let countSql = 8768;
+
+function incrementarSql() {
+    countSql++;
+    numeroSql.innerText = countSql
+}
+
+let botaoLikeHtml = document.getElementById("likeHtml")
+let numeroHtml = document.getElementById("contagemHtml")
+
+let countHtml = 5446;
+
+function incrementarHtml() {
+    countHtml++;
+    numeroHtml.innerText = countHtml
+}
+
+
+let botaoLikeJava = document.getElementById("likeJava")
+let numeroJava = document.getElementById("contagemJava")
+
+let countJava = 10265;
+
+function incrementarJava() {
+    countJava++;
+    numeroJava.innerText = countJava
+}
+
+let botaoLikeDocker = document.getElementById("likeDocker")
+let numeroDocker = document.getElementById("contagemDocker")
+
+let countDocker = 1356;
+
+function incrementarDocker() {
+    countDocker++;
+    numeroDocker.innerText = countDocker
+}
+
+let botaoLikePhp = document.getElementById("likePhp")
+let numeroPhp = document.getElementById("contagemPhp")
+
+let countPhp = 4648;
+
+function incrementarPhp() {
+    countPhp++;
+    numeroPhp.innerText = countPhp
+}
