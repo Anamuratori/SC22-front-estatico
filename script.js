214f7cfd8b6c042313b1947a4cc04f762cf318e4
Pietro Zardini <pizardini@gmail.com>
2023-06-09 18:58:11 -0300

    refatoração dos likes

--- script.js
+++ script.js
@@ -1,61 +1,18 @@
-let botaoLikeC = document.getElementById("likeC");
-let numeroC = document.getElementById("contagemC");
+function contadorLikes(botaoId, contadorId, numInicial) {
+    const botao = document.getElementById(botaoId);
+    const contador = document.getElementById(contadorId);
 
-let countC = 2656;
+    let conta = numInicial;
 
-function incrementarC() {
-    countC++;
-    numeroC.innerText = countC
+    botao.addEventListener('click', () => {
+        conta++;
+        contador.innerText = conta;
+    })
 }
 
-
-let botaoLikeSql = document.getElementById("likeSql")
-let numeroSql = document.getElementById("contagemSql")
-
-let countSql = 8768;
-
-function incrementarSql() {
-    countSql++;
-    numeroSql.innerText = countSql
-}
-
-let botaoLikeHtml = document.getElementById("likeHtml")
-let numeroHtml = document.getElementById("contagemHtml")
-
-let countHtml = 5446;
-
-function incrementarHtml() {
-    countHtml++;
-    numeroHtml.innerText = countHtml
-}
-
-
-let botaoLikeJava = document.getElementById("likeJava")
-let numeroJava = document.getElementById("contagemJava")
-
-let countJava = 10265;
-
-function incrementarJava() {
-    countJava++;
-    numeroJava.innerText = countJava
-}
-
-let botaoLikeDocker = document.getElementById("likeDocker")
-let numeroDocker = document.getElementById("contagemDocker")
-
-let countDocker = 1356;
-
-function incrementarDocker() {
-    countDocker++;
-    numeroDocker.innerText = countDocker
-}
-
-let botaoLikePhp = document.getElementById("likePhp")
-let numeroPhp = document.getElementById("contagemPhp")
-
-let countPhp = 4648;
-
-function incrementarPhp() {
-    countPhp++;
-    numeroPhp.innerText = countPhp
-}
+contadorLikes("likeC", "contagemC", 2656)
+contadorLikes("likeSql", "contagemSql", 8768)
+contadorLikes('likeHtml', 'contagemHtml', 5446);
+contadorLikes("likeJava", "contagemJava", 10265)
+contadorLikes("likeDocker", "contagemDocker", 1356)
+contadorLikes("likePhp", "contagemPhp", 4648)
